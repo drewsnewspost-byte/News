2e39c8af524edce3a2dfe8ba0dd0d44fb894e3d5
Drew Rakosi <drewsnewspost@gmail.com>
2026-08-22 21:35:27 +0000

    Stop shipping leftover Next.js build folders.

--- .next-ia-check/static/F0C-sVxLcb0ZMlArBakzv/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[section]","\u002F[section]\u002F[year]\u002F[month]\u002F[slug]","\u002Fhoroscope\u002F[sign]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
